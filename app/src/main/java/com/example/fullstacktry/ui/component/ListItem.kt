3a2e947068d4e7268947b0cbf4138b86e25b1fd9
mansao01 <novrilukman15@gmail.com>
2023-08-02 13:11:56 +0700

    change garbage position

--- app/src/main/java/com/example/fullstacktry/ui/component/ListItem.kt
+++ app/src/main/java/com/example/fullstacktry/ui/component/ListItem.kt
@@ -1,6 +1,11 @@
 package com.example.fullstacktry.ui.component
 
+import android.graphics.Paint.Align
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -10,7 +15,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.fullstacktry.ui.screen.home.HomeViewModel
 
@@ -29,29 +36,38 @@ fun ListItem(
             .padding(top = 4.dp)
             .padding(horizontal = 16.dp)
     ) {
-        Column {
-            Text(
-                text = name, modifier = modifier
-                    .padding(horizontal = 8.dp)
-                    .padding(top = 4.dp)
-            )
-            Text(
-                text = age.toString(),
-                modifier = modifier
-                    .padding(horizontal = 8.dp)
-                    .padding(top = 4.dp)
-            )
-            Text(
-                text = address, modifier = modifier
-                    .padding(horizontal = 8.dp)
-                    .padding(top = 4.dp)
-            )
+
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Column {
+                Text(
+                    text = name, modifier = modifier
+                        .padding(horizontal = 8.dp)
+                        .padding(top = 4.dp)
+                )
+                Text(
+                    text = age.toString(),
+                    modifier = modifier
+                        .padding(horizontal = 8.dp)
+                        .padding(top = 4.dp)
+                )
+                Text(
+                    text = address, modifier = modifier
+                        .padding(horizontal = 8.dp)
+                        .padding(top = 4.dp)
+                )
+
+            }
+//            add space for make Icon button in the end of the row
+            Spacer(Modifier.weight(1f).fillMaxHeight())
 
             IconButton(onClick = {
                 homeViewModel.deleteProfile(id)
                 homeViewModel.getUserList()
             }) {
-                Icon(imageVector = Icons.Default.Delete, contentDescription = "delete data")
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "delete data",
+                )
             }
         }
     }
